963d629f1344e2d254357415dc971fd66511771f
msung99 <msung6924@naver.com>
2024-02-24 15:29:13 +0900

    feat: Series Component Grid 배치 및 스타일링

--- src/components/series-list/SeriesList.jsx
+++ src/components/series-list/SeriesList.jsx
@@ -5,37 +5,71 @@ import styled from "styled-components";
 
 const SeriesList = ({ seriesList, totalCount }) => {
   return (
-    <div>
+    <PageContent>
       <Title>Series.</Title>
       <Description>{totalCount} series found.</Description>
       <SeriesContainer>
         {seriesList.map((series) => (
           <SeriesStyle key={kebabCase(series.fieldValue)}>
-            <Link
-              style={{ textDecoration: "none", color: "#cdd4d9" }}
-              to={`/series/${kebabCase(series.fieldValue)}/`}
-            >
-              {kebabCase(series.fieldValue)}
-            </Link>
+            <TitleSection>
+              <Link
+                style={{ textDecoration: "none", color: "#cdd4d9" }}
+                to={`/series/${kebabCase(series.fieldValue)}/`}>
+                {series.fieldValue}
+              </Link>
+            </TitleSection>
+            <DescriptionSection>
+                안녕하세요!
+            </DescriptionSection>
           </SeriesStyle>
         ))}
       </SeriesContainer>
-    </div>
+    </PageContent>
   );
 };
 
-const SeriesContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  gap: 16px;
-  justify-content: space-between;
+const PageContent = styled.div`
+  position: relative;
+  z-index: 1;
 `;
 
+const SeriesContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 29px;
+  margin-top: 60px;
+  row-gap: 64px;
+  margin: 0 auto;
+
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+
 const SeriesStyle = styled.div`
-  background-color: #2c3e50;
-  padding: 16px;
-  border-radius: 8px;
-  text-align: center; /* Optional: Center the text within each box */
+  display: flex;
+  flex-direction: column;
+  background-color: #2a2a2a;
+  padding: 0;
+  border-radius: 20px;
+  margin-bottom: 0;
+`;
+
+
+const TitleSection = styled.h1`
+  font-size: 25px;
+  margin-bottom: 50px;
+  margin-left: 15px;
+`;
+
+const DescriptionSection = styled.p`
+  background-color: #3a3a3a;
+  color: white;
+  flex-grow: 1;
+  margin: 0;
+  height: 50px;
+  border-radius: 0 0 20px 20px;
 `;
 
 const Title = styled.h1`
