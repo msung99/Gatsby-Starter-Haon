d0feb99436efad560218e34e0eef33b78b776022
msung99 <msung6924@naver.com>
2024-02-24 12:21:23 +0900

    fix: Series.SeriesContent 를 Series.Content 로 수정

--- src/components/series/series-content/index.jsx
+++ src/components/series/series-content/index.jsx
@@ -1,6 +1,23 @@
+import React from "react";
 import styled from "styled-components";
+import PostList from "../../postlist";
 
-const SeriesContent = styled.div`
+const SeriesContent = ({totalCount, posts}) => {
+    const seriesDescription = `${totalCount} post${
+        totalCount === 1 ? "" : "s"} found.`
+
+    return (
+        <div>
+            <Count>{seriesDescription}</Count>       
+            <PostList posts={posts}/>
+        </div>
+    )
+}
+
+const Count = styled.h3`
+  color: white;
+  font-size: 22px;
+  margin-bottom: 50px;
 `
 
 export default SeriesContent
--- src/components/series/series-header/index.jsx
+++ src/components/series/series-header/index.jsx
@@ -1,6 +1,32 @@
+import React from "react";
 import styled from "styled-components";
 
-const SeriesHeader = styled.div`
+const SeriesHeader = ({seriesName}) => {
+    return (
+        <HeaderStyle>
+            <Name>{seriesName}.</Name>
+        </HeaderStyle>
+    )
+}
+
+const HeaderStyle = styled.div`
+  margin-bottom: 50px;
+  border-bottom: 1px solid #282828;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
+
+const Title = styled.h1`
+  color: white;
+  font-size: 20px;
+`
+
+const Name = styled.h2`
+  color: white;
+  font-size: 40px;
+  font-style: italic;
+`
+
 
 export default SeriesHeader
