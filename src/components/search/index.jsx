91fc16dc4152420e343d80c7a182e339cdb1f59d
msung99 <msung6924@naver.com>
2024-02-25 17:43:50 +0900

    refactor: ResultCount 애니메이션 1s 변경

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -7,9 +7,7 @@ const Search = ({ onChange, placeholder, count }) => {
 
   return (
     <SearchContainer>
-      <InputLeftElement>
-        <IoIosSearch size={20} style={{ marginRight: 10, color: '#888' }} />
-      </InputLeftElement>
+      <Title>Search.</Title>
       <Input
         type="text"
         onChange={onChange}
@@ -34,6 +32,12 @@ const SearchContainer = styled.div`
   cursor: text;
 `;
 
+const Title = styled.h1`
+  color: #fff;
+  font-size: 40px;
+  font-style: italic;
+`
+
 const Input = styled.input`
   font-size: 18px;
   color: #fff;
@@ -41,11 +45,14 @@ const Input = styled.input`
   height: 50px;
   border: none;
   border-bottom: solid #888888 2px;
-  padding-left: 40px; 
+  padding-left: 30px;
   background-color: #333;
   z-index: 5;
-  border-radius: 5px; 
-  transition: border-bottom 1.5s ease;
+  border-radius: 5px;
+  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" fill="%23888" viewBox="0 0 24 24"><path d="M21 19l-5-5a9 9 0 1 0-1.4 1.4l5 5 1.4-1.4zM17 9a7 7 0 1 1-14 0 7 7 0 0 1 14 0z"/></svg>');
+  background-repeat: no-repeat;
+  background-position: 5px center; 
+  background-size: 20px; 
 
   &::placeholder {
     color: #888;
@@ -53,39 +60,30 @@ const Input = styled.input`
 
   &:focus {
     outline: none;
-    border-bottom: solid #ffffff 2px;
   }
-`;
-
-const InputLeftElement = styled.div`
-  position: absolute;
-  top: 15px;
-  left: 10px;
-  display: flex;
-  align-items: center;
 `;
 
 const Underline = styled.span`
   position: absolute;
-  bottom: 0;
-  left: 0;
+  left: 0;  
   width: ${({ isFocused }) => (isFocused ? '100%' : '0')};
-  height: 2px; 
+  height: 2px;
   background-color: #fff;
   opacity: ${({ isFocused }) => (isFocused ? 1 : 0)};
-  transition: width 1.5s ease, opacity 1.5s ease; 
+  transition: width 1s ease, opacity 1s ease-in-out; 
 `;
 
 const ResultCount = styled.div`
   position: absolute;
-  left: 10px;
-  top: 80px;
+  top: 120px;
   font-size: 22px;
+  margin-top: 30px;
   color: #fff;
   font-family: 'Helvetica Neue', sans-serif;
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   opacity: ${({ count }) => (count === 0 ? 0 : 1)};
-  transition: opacity 0.3s ease;
+  transition: opacity 1s ease-in-out, visibility 1s ease-in-out; /* Adjust the duration for appearance */
+  visibility: ${({ count }) => (count === 0 ? 'hidden' : 'visible')}; /* Hide when count is 0 */
 
   &:before {
     content: '${props => (props.count === 0 ? 'No matching posts were found.' : (props.count === 1 ? 'There is 1 post found.' : `There are ${props.count} posts found.`))}';
