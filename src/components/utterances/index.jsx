f92b0979203c7fab86041935b14fed8d2d875096
msung99 <msung6924@naver.com>
2024-02-26 00:34:57 +0900

    fix: Utterances 쿼리 추출

--- src/components/utterances/index.jsx
+++ src/components/utterances/index.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
-import { repo } from "../../../gatsby-config"
+import { useStaticQuery, graphql } from 'gatsby';
 
 const Utterances = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          repo
+        }
+      }
+    }
+  `);
+
+  const repo = data.site.siteMetadata.repo;
+
   return (
-    <CommentWrapper>
+    <CommentWrapper
       ref={(elem) => {
         if (!elem) {
           return;
         }
+        <h1>repo</h1>
         const scriptElem = document.createElement('script');
         scriptElem.src = 'https://utteranc.es/client.js';
         scriptElem.async = true;
-        scriptElem.setAttribute('repo', {repo});
+        scriptElem.setAttribute('repo', repo);
         scriptElem.setAttribute('issue-term', 'pathname');
         scriptElem.setAttribute('theme', 'github-dark');
         scriptElem.setAttribute('label', 'blog-comment');
         scriptElem.crossOrigin = 'anonymous';
         elem.replaceChildren(scriptElem);
       }}
-    </CommentWrapper>
+    />
   );
 };
 
-const CommentWrapper = styled.section`
-`;
+const CommentWrapper = styled.section``;
 
 export default Utterances;
---
+++ src/pages/404.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import { TbError404 } from "react-icons/tb";
+import styled from "styled-components";
+import PageLayout from "../components/layout/page-component";
+import Seo from "../components/seo";
+
+const NotFoundWrapper = styled.div`
+  text-align: center;
+  margin: 50px auto;
+`;
+
+const NotFoundTitle = styled.h1`
+  color: #333;
+  font-size: 3rem;
+`;
+
+const NotFoundIcon = styled(TbError404)`
+  margin: 20px 0;
+  color: #333;
+`;
+
+const NotFoundText = styled.p`
+  color: #555;
+  font-size: 1.2rem;
+`;
+
+const NotFoundLink = styled(Link)`
+  color: #007bff;
+  text-decoration: underline;
+`;
+
+const NotFoundPage = () => (
+  <PageLayout>
+    <NotFoundWrapper>
+      <NotFoundTitle>404: Not Found</NotFoundTitle>
+      <NotFoundIcon size="50" />
+      <NotFoundText>
+        You just hit a route that doesn&#39;t exist... the sadness.{" "}
+        <NotFoundLink to="/">Go back to the home page</NotFoundLink>.
+      </NotFoundText>
+    </NotFoundWrapper>
+  </PageLayout>
+);
+
+export const Head = () => <Seo title="404: Not Found" />;
+
+export default NotFoundPage;
