fc7acdcb2c27cfa6f03ac9595e7d50bf23ffdb5a
msung99 <msung6924@naver.com>
2024-03-07 20:46:12 +0900

    refactor: ContentWrapper, HtmlWrapper 상수로 분리하여 재사용성 개선

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,6 +9,9 @@ import { SiVelog } from "react-icons/si";
 import { MdOutlineEmail } from "react-icons/md";
 import Profile from "../components/profile/index.jsx";
 import Seo from "../components/seo/index.jsx";
+import PostContent from "../components/post/post-content/index.jsx";
+import { ContentWrapper, HtmlWrapper } from "../components/post/post-content/post-style/index.jsx";
+import TableOfContents from "../components/toc/index.jsx";
 
 const AboutTemplate = ({ data, location}) => {
   const { frontmatter, html } = data.markdownRemark;
@@ -17,6 +20,9 @@ const AboutTemplate = ({ data, location}) => {
   const author = data.site.siteMetadata.author
   const siteUrl = data.site.siteMetadata.siteUrl
   const keywords = data.site.siteMetadata.keywords
+
+  const post = data.markdownRemark;
+  const toc = post.tableOfContents;
 
   return (
     <PageLayout>
@@ -30,7 +36,12 @@ const AboutTemplate = ({ data, location}) => {
         ))}
       </SocialLinks>
       <Line/>
-      <MarkdownContent dangerouslySetInnerHTML={{ __html: html || '' }} />
+      <ContentWrapper>
+        <TableOfContents content={toc}/>
+        <HtmlWrapper>
+          <section dangerouslySetInnerHTML={{ __html: html}} itemProp="postContent" />
+        </HtmlWrapper>
+      </ContentWrapper>
       <Utterances/>
     </PageLayout>
   );
@@ -52,154 +63,6 @@ const EmojiLink = styled.span`
   cursor: pointer;
   color: ${props => props.theme.emoji};
 `;
-
-const MarkdownContent = styled.div`
- h1 {
-  font-size: 2rem;
-  color: ${props => props.theme.post.content.text};
-  margin-top: 70px;
-  margin-bottom: 40px;
-  word-break: break-all;
-  font-weight: 700;
-  line-height: 130%;
-}
-
- h2 {
-  font-size: 1.7rem;
-  color: ${props => props.theme.post.content.text};
-  margin-top: 70px;
-  margin-bottom: 40px;
-  word-break: break-all;
-  font-weight: 700;
-  line-height: 130%;
-}
-
- h3 {
-  font-size: 1.4rem;
-  color: ${props => props.theme.post.content.text};
-  margin-top: 70px;
-  margin-bottom: 40px;
-  word-break: break-all;
-  font-weight: 700;
-  line-height: 130%;
-}
-
- h4 {
-  font-size: 1.1rem;
-  color: ${props => props.theme.post.content.text};
-  margin-top: 40px;
-  margin-bottom: 40px;
-  word-break: break-all;
-  font-weight: 700;
-  line-height: 110%;
-}
-
- h5 {
-  font-size: 0.9rem;
-  color: ${props => props.theme.post.content.text};
-  margin-top: 40px;
-  margin-bottom: 40px;
-  word-break: break-all;
-  font-weight: 700;
-}
-
- h6 {
-  font-size: 0.7rem;
-  color: ${props => props.theme.post.content.text};
-  margin-top: 40px;
-  margin-bottom: 40px;
-  font-weight: 700;
-}
-
- p {
-  font-size: 16px;
-  line-height: 180%; 
-  color: ${props => props.theme.post.content.text};
-  margin-bottom: 35px;
-  word-break: break-all;
-
-  span {
-    margin-top: 40px;
-    margin-bottom: 40px;
-    font-size: 100px;
-  }
-}
-
-
-blockquote {
-  padding-left: 18px;
-  padding-right: 20px;
-  padding-top: 25px;
-  padding-bottom: 1px;
-  margin-bottom: 50px; 
-  margin-top: 50px;
-  background-color: ${props => props.theme.post.content.blockquote.body};
-  line-height: 170%;
-  color: ${props => props.theme.post.content.blockquote.text};
-  border-left: 6px solid ${props => props.theme.post.content.blockquote.left};
-  word-break: break-all;
-}
-
-hr {
-  height: 2px;
-  border: 0;
-  background-color: ${props => props.theme.post.content.hr};
-  margin-top: 50px;
-  margin-bottom: 50px;
-  word-break: break-all;
-}
-
-a {
-  color: ${props => props.theme.post.content.a};
-  word-break: break-all;
-}
-
-img {
-  width: 100%;
-  height: 100%;
-  object-fit: fill;
-  margin-top: 40px;
-  margin-bottom: 40px;
-}
-
-ul {
-  line-height: 180%; 
-  word-break: break-all;
-}
-
-li {
-  color: ${props => props.theme.post.content.li};
-  list-style-type: disc;
-  margin-left: 20px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
-
-
-
-.gatsby-highlight {
-  font-size: 13.5px;
-  margin-bottom: 80px;
-  word-break: break-all;
-
-  .language-text {
-    background-color: ${props => props.theme.post.content.blockquote.body};
-    font-size: 13.5px;
-    color: ${props => props.theme.post.content.blockquote.text};
-  }
-}
-
-  .language-text {
-    background-color: ${props => props.theme.post.content.language.bg};
-    padding: -10px;
-    font-size: 15px;
-    color: ${props => props.theme.post.content.language.text};
-    width: 100%;
-    height: 100%;
-    word-break: break-all;
-  }
-}
-`
 
 const socialEmojis = {
   github: <FaGithub className="icon" size="30" />,
@@ -233,6 +96,7 @@ export const pageQuery = graphql`
       }
     }
     markdownRemark(fields: { slug: { eq: "/default/about/" } }) {
+      tableOfContents(maxDepth: 3)
       frontmatter {
         title
         description
