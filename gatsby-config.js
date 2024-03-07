b14f228bc920110bc916dbf13df775c3c0c14972
msung99 <msung6924@naver.com>
2024-03-07 13:28:43 +0900

    chore: Favicon 프로필 연동

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/profile.png`,
       },
     },
     `gatsby-plugin-styled-components`,
