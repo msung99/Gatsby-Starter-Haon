f83c256c500a4fe7150d2614037656ab475ed63f
Haon <msung6924@naver.com>
2024-05-16 23:40:16 +0900

    #45 refactor: meta-config.js 설정파일 생성

---
+++ meta-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: `haon.theme`,
+    description: `Hello! This is a tech blog theme using Gatsby 🤩`,
+    author: `Haon`,
+    siteUrl: `https://gatsby-starter-haon.netlify.app`,
+    keywords: [`server`, `backend`, `gatsby`], 
+    repo: 'msung99/Gatsby-Starter-Haon',
+    socialLinks: { 
+      github: 'https://github.com/msung99', 
+      instagram: 'https://www.instagram.com/iminseong920/', 
+      facebook: 'https://www.facebook.com/', 
+      linkedin: 'https://www.linkedin.com/',
+      velog: 'https://velog.io/@msung99',
+      email: 'msung6924@naver.com',
+    }
+}
