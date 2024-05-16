f125d0d3dfc0dbcd60510d69af9530420fdf1978
Haon <msung6924@naver.com>
2024-05-17 00:41:53 +0900

    #45 feat: gtag 설정 기능 추가

--- meta-config.js
+++ meta-config.js
@@ -5,6 +5,7 @@ module.exports = {
     siteUrl: `https://gatsby-starter-haon.netlify.app`,
     keywords: [`server`, `backend`, `gatsby`], 
     repo: 'msung99/Gatsby-Starter-Haon',
+    gtag: 'G-CD9E7GB2ED', // with. Google Analytics
     socialLinks: { 
       github: 'https://github.com/msung99', 
       instagram: 'https://www.instagram.com/iminseong920/', 
