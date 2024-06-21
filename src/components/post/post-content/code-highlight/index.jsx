81f69a7b1782c86cfbcfc09265c3fc67200812c5
Haon <msung6924@naver.com>
2024-06-21 17:10:42 +0900

    #33 feat: 테마 전환 즉시 코드블럭 컬러 변경

--- src/components/post/post-content/code-highlight/index.jsx
+++ src/components/post/post-content/code-highlight/index.jsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from "react";
 import DarkMode from "./dark";
 import LightMode from "./light";
-import { getValueFromLocalStorage, setValueToLocalStorage } from "../../../../utils/localStorage";
-
+import { getValueFromLocalStorage } from "../../../../utils/localStorage";
 
 const CodeHighLight = () => {
-    const [isDarkMode, setIsDarkMode] = useState(getValueFromLocalStorage('isDarkMode'));
-  
-    useEffect(() => {
-      setValueToLocalStorage('isDarkMode', isDarkMode);
-      document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
-    }, [isDarkMode]);
-  
-    return (
-      <>
-        {isDarkMode ? <DarkMode/> : <LightMode/>}
-      </>
-    );
-  }
-  
-  export default CodeHighLight;
+  const [isDarkMode, setIsDarkMode] = useState(getValueFromLocalStorage('isDarkMode'));
+
+  useEffect(() => {
+    const handleThemeChange = () => {
+      setIsDarkMode(getValueFromLocalStorage('isDarkMode'));
+    };
+
+    // 테마 변경 이벤트 리스너 등록
+    window.addEventListener('themeChange', handleThemeChange);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('themeChange', handleThemeChange);
+    };
+  }, []);
+
+  return (
+    <>
+      {isDarkMode ? <DarkMode/> : <LightMode/>}
+    </>
+  );
+}
+
+export default CodeHighLight;
